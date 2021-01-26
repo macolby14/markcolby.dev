571fa36f3f5dfebe6537ff14bc3d9c6acf4c3fbe
Mark Colby <macolby14@gmail.com>
2021-01-25 23:06:13 -1000

    starter code, eslint, and prettier

---
+++ .eslintrc.js
@@ -0,0 +1,28 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+    node: true,
+  },
+  extends: [
+    "plugin:react/recommended",
+    "airbnb",
+    "prettier",
+    "prettier/@typescript-eslint",
+    "prettier/react",
+  ],
+  parser: "@typescript-eslint/parser",
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 12,
+    sourceType: "module",
+  },
+  plugins: ["react", "@typescript-eslint"],
+  rules: {
+    "react/react-in-jsx-scope": "off",
+    "react/jsx-filename-extension": ["warn", { extensions: [".tsx", ".jsx"] }],
+    "no-unused-vars": "warn",
+  },
+};
---
+++ .prettierrc.js
@@ -0,0 +1,3 @@
+// Local prettier configuration to override any Vscode user settings.
+// None currently set but tells vscode we are using prettier for formatting
+module.exports = {};
