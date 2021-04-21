222a3d1981ad9737b2c15cd441da141c49873734
Mark Colby <macolby14@gmail.com>
2021-04-21 10:53:54 -1000

    add global css variables

--- styles/globalStyles.ts
+++ styles/globalStyles.ts
@@ -53,13 +53,67 @@ table {
 }
 
 
+/* CSS Variables*/
+:root{
+  --color-text: hsl(0deg,0%,0%); //black
+  --color-background: hsl(0deg,0%,100%); //white
+  --font-family: -apple-system, BlinkMacSystemFont, Segoe UI, Roboto, Oxygen,
+    Ubuntu, Cantarell, Fira Sans, Droid Sans, Helvetica Neue, sans-serif;
+  --font-weight-light: 300;
+  --font-weight-regular: 400;
+  --font-weight-bold: 700;
+  --color-primary: blue;
 
-/* Custom global styles */
+  // Typography scale is Perfect Fourth from https://type-scale.com/
+
+}
+
+
+/* Global Styles */
 *, *::before, *::after {
     box-sizing: border-box;
+    line-height: 1.45;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: auto;
   }
-  html {
-    font-size: 18px;
+
+  html, body{
+    height: 100%;
+  }
+
+  #__next{
+    isolation: isolate;
+  }
+
+  body{
+    color: var(--color-text);
+    background: var(--color-background);
+  }
+
+  a:focus{
+    outline: 5px auto var(--color-primary);
+  }
+
+  body,input,button,select,option{
+    font-family: var(--font-family);
+    font-weight: var(--font-weight-light);
+  }
+
+  h1, h2, h3, h4, h5, h6, strong{
+    font-weight: var(--font-weight-bold);
+  }
+
+  h1, h2, h3, h4, h5, h6, p {
+    text-rendering: optimizeLegibility;
+  }
+
+  p{
+    margin-bottom: 1.5em;
+    font-size: 1.125rem;
+  }
+
+  em{
+    font-style: italic;
   }
 
 `;
