ac5ae69c9bafb9674f00445608aea9a5d5972aa4
Mark Colby <macolby14@gmail.com>
2021-04-21 11:32:04 -1000

    add Box component

---
+++ components/Box.tsx
@@ -0,0 +1,26 @@
+// eslint-disable-next-line no-use-before-define
+import React from "react";
+import styled from "styled-components";
+
+interface WrapperProps {
+  maxWidth: number;
+  gutter: number;
+}
+
+const Wrapper = styled.div<WrapperProps>`
+  margin: 0 auto;
+  max-width: ${(props) => props.maxWidth}px;
+  padding: ${(props) => props.gutter}px;
+`;
+
+interface Props extends WrapperProps {
+  children: React.ReactNode;
+}
+
+export function Box({ children, maxWidth, gutter }: Props) {
+  return (
+    <Wrapper maxWidth={maxWidth} gutter={gutter}>
+      {children}
+    </Wrapper>
+  );
+}
