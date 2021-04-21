7bbf7ad3c87b02f5ddedd9fdc005b02f658c6195
Mark Colby <macolby14@gmail.com>
2021-04-21 13:51:11 -1000

    add social media icons to intro section

--- components/Intro.tsx
+++ components/Intro.tsx
@@ -1,27 +1,70 @@
 import styled from "styled-components";
 import { Box } from "./Box";
 
-const Wrapper = styled.section``;
+const LinkBreak = styled.div`
+  --line-margin-left: -64px;
+  --icon-padding-sides: 8px;
 
-const Line = styled.hr`
-  max-width: 400px;
-  margin: 8px 0;
+  display: flex;
+  align-items: center;
+  margin-top: 24px;
+  margin-left: var(--line-margin-left);
+  max-width: 600px;
+  i {
+    color: var(--color-text);
+    margin: 0 var(--icon-padding-sides);
+    font-size: var(--font-size-5);
+  }
+`;
+
+const Line = styled.hr<{ growRatio: number }>`
+  flex-grow: 1;
+  margin: 0;
   border-top: none;
   border-left: none;
   border-right: none;
   border-bottom: 2px solid black;
+
+  //Make first hr icons align with text body with width of --line-margin-left
+  &:first-of-type {
+    max-width: calc(-1 * var(--line-margin-left) - var(--icon-padding-sides));
+  }
 `;
 
 export function Intro() {
   return (
-    <Wrapper>
-      <Box maxWidth={1000} gutter={32}>
-        <h1>Hi, I'm Mark</h1>
+    <>
+      <Box maxWidth={1000} gutter={96}>
+        <h1>Hi, I&apos;m Mark</h1>
         <h4>Code Enthusiast</h4>
         <h4>Submarine Veteran</h4>
         <h4>Lifelong Learner</h4>
-        <Line />
+        <LinkBreak>
+          <Line growRatio={0.1} />
+          <a
+            href="https://www.linkedin.com/in/macolby14/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <i className="fab fa-linkedin-in" />
+          </a>
+          <a
+            href="https://github.com/macolby14"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <i className="fab fa-github" />
+          </a>
+          <a
+            href="mailto:macolby14@gmail.com"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <i className="fas fa-envelope" />
+          </a>
+          <Line growRatio={1} />
+        </LinkBreak>
       </Box>
-    </Wrapper>
+    </>
   );
 }
