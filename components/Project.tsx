2ad08f5b96969af86a92cfdb3bf9ae423fdda79d
Mark Colby <macolby14@gmail.com>
2021-04-23 11:01:56 -1000

    add responsive image to Project component

--- components/Project.tsx
+++ components/Project.tsx
@@ -12,14 +12,17 @@ const Wrapper = styled.article`
   justify-content: center;
 `;
 
-const ProjectImage = styled.div`
-  flex: 0 1 200px;
-  border: 1px solid black;
-  height: 200px;
+const ImageContainer = styled.div`
+  flex: 0 1 300px;
+
+  img {
+    max-width: 100%;
+    height: auto;
+  }
 `;
 
 const ProjectDescription = styled.div`
-  flex: 1 1 600px;
+  flex: 1 1 400px;
 
   padding: 0 16px;
 
@@ -31,16 +34,20 @@ const ProjectDescription = styled.div`
 
 export function Project() {
   return (
-    <Wrapper>
-      <ProjectImage>Image</ProjectImage>
-      <ProjectDescription>
-        <h3>Jogging Beats</h3>
-        <p>
-          A site that uses the Spotify API to allow users to quickly generate
-          playlists to a certain running beats per minute.
-        </p>
-        <p>This is the tech stack</p>
-      </ProjectDescription>
-    </Wrapper>
+    <a href="/test">
+      <Wrapper>
+        <ImageContainer>
+          <img src="/Home_Screenshot.jpg" alt="Home Page" />
+        </ImageContainer>
+        <ProjectDescription>
+          <h3>Jogging Beats</h3>
+          <p>
+            A site that uses the Spotify API to allow users to quickly generate
+            playlists to a certain running beats per minute.
+          </p>
+          <p>This is the tech stack</p>
+        </ProjectDescription>
+      </Wrapper>
+    </a>
   );
 }
