f27b544192dc26aa8160e986c4ad1a992d8a2205
Mark Colby <macolby14@gmail.com>
2021-04-23 13:05:51 -1000

    read filenames from folder with staticprops

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,16 @@
+import fs from "fs";
+import path from "path";
 import Head from "next/head";
 import { About } from "../components/About";
 import { Box } from "../components/Box";
 import { Intro } from "../components/Intro";
 import { Projects } from "../components/Projects";
 
-export default function Home() {
+interface Props {
+  filenames: string[];
+}
+
+export default function Home({ filenames }: Props) {
   return (
     <div>
       <Head>
@@ -25,4 +31,33 @@ export default function Home() {
       </footer>
     </div>
   );
+}
+
+export async function getStaticProps() {
+  const postsDirectory = path.join(process.cwd(), "projects");
+  const filenames = fs.readdirSync(postsDirectory);
+
+  console.log(filenames);
+
+  return { props: { filenames } };
+
+  // const posts = filenames.map((filename) => {
+  //   const filePath = path.join(postsDirectory, filename);
+  //   const fileContents = fs.readFileSync(filePath, "utf8");
+
+  //   // Generally you would parse/transform the contents
+  //   // For example you can transform markdown to HTML here
+
+  //   return {
+  //     filename,
+  //     content: fileContents,
+  //   };
+  // });
+  // // By returning { props: posts }, the Blog component
+  // // will receive `posts` as a prop at build time
+  // return {
+  //   props: {
+  //     posts,
+  //   },
+  // };
 }
