632dbdcff040d3147feb17656f02fcd4c17e8bcd
Mark Colby <macolby14@gmail.com>
2021-04-23 16:30:50 -1000

    delete unused dynamic project name route

--- pages/project/[name].tsx
+++
@@ -1,8 +0,0 @@
-import { useRouter } from "next/dist/client/router";
-
-export default function Project() {
-  const router = useRouter();
-  const { name } = router.query;
-
-  return <main>Project Page: {name}</main>;
-}
