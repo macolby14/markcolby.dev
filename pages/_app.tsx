85e7129df20a3f5440ce59fe2aae3ab4856e68c8
Mark Colby <macolby14@gmail.com>
2025-01-25 15:29:06 -0500

    Add vercel analytics

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,5 +1,6 @@
 import type { AppProps } from "next/app";
 import Head from "next/head";
+import { Analytics } from "@vercel/analytics/react"; // eslint-disable-line import/no-unresolved
 import { GlobalStyles } from "../styles/globalStyles";
 
 function MyApp({ Component, pageProps }: AppProps) {
@@ -10,6 +11,7 @@ function MyApp({ Component, pageProps }: AppProps) {
         <meta name="viewport" content="width=device-width" />
         <title>Mark Colby</title>
       </Head>
+      <Analytics />
       {/* eslint-disable-next-line react/jsx-props-no-spreading */}
       <Component {...pageProps} />
       <GlobalStyles />
